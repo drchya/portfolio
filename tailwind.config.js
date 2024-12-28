2931d69de848253b829c9f259cb451006bffed64
drchya <rdwic15@gmail.com>
2024-12-29 03:19:40 +0700

    structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'ui-sans-serif', 'system-ui'],
+        poppins: ['Poppins', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
 }
