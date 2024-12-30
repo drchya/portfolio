4803814f01aadd09fc1e66dedee133159cfd8b31
drchya <rdwic15@gmail.com>
2024-12-30 11:45:32 +0700

    FEAT: fix design and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,22 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '16px'
+      padding: '16px',
     },
     extend: {
+      colors: {
+        primary: {
+          100: "#FFE1DB",
+          200: "#FFC2B8",
+          300: "#FFA494",
+          400: "#FF8571",
+          500: "#FF674D",
+          600: "#CC341A",
+          700: "#990100",
+          800: "#660000",
+          900: "#4D0000",
+        },
+      },
       fontFamily: {
         inter: ['Inter', 'ui-sans-serif', 'system-ui'],
         poppins: ['Poppins', 'ui-sans-serif', 'system-ui'],
